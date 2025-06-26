807ffa2bbf5f6fa596b9ae9dba7a651809d6c3a3
Jayce Walzer <jwalzer3@gatech.edu>
2025-06-26 12:18:00 -0400

    Update run.py

--- Server/run.py
+++ Server/run.py
@@ -10,4 +10,4 @@ os.makedirs(instance_path, exist_ok=True)
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=False, port=5000)
+    app.run(host='0.0.0.0', debug=False, port=5000)
