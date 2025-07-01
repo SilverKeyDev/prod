0aa20ea01b0cda90453710e0296eb58e85419490
Jayce Walzer <walzerjayce@gmail.com>
2025-07-01 18:47:14 -0400

    jksbg

--- Server/run.py
+++ Server/run.py
@@ -1,7 +1,11 @@
 from app import create_app, db
-from flask_cors import CORS
+from flask import send_from_directory
 
 app = create_app()
+
+@app.route('/assets/<path:filename>')
+def serve_assets(filename):
+    return send_from_directory('../Client/dist/assets', filename)
 
 with app.app_context():
     db.create_all()
