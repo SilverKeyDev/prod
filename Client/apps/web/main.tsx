62e4f957cbfc824c37a692030f67b1f6cd553b22
Jayce Walzer <walzerjayce@gmail.com>
2025-10-01 15:34:58 -0400

    udpate

--- Client/apps/web/main.tsx
+++ Client/apps/web/main.tsx
@@ -11,12 +11,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter
-      future={{
-        v7_startTransition: true,
-        v7_relativeSplatPath: true,
-      }}
-    >
+    <BrowserRouter>
       <CoreProviders>
         <App />
       </CoreProviders>
